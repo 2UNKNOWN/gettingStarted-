808e441895cdcdc4183aec833a8d8c43d49cfd30
Ahmad <ahmadaldali6021@gmail.com>
2021-10-19 14:05:05 -0500

    testing

---
+++ gettingStarted/src/gettingStarted/GettingStarted.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gettingStarted;
+
+/**
+ * @author AldaliA
+ *
+ */
+public class GettingStarted {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//fixed A COMMENT
+		// another one 
+		System.out.println("hey");
+	}
+
+}
